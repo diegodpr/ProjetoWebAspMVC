519703e6b9dd5cb61af665b40ab84d5e5369b1d3
diego paulo ramos <diegop_ramos@hotmail.com>
2021-06-10 16:39:03 -0300

    Criando Segunda Migration

--- WebApplication1/Models/Department.cs
+++ WebApplication1/Models/Department.cs
@@ -9,5 +9,26 @@ namespace WebApplication1.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; }
+
+        public Department()
+        {
+        }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime inicial, DateTime final)
+        {
+            return Sellers.Sum(sl => sl.TotalSales(inicial, final));
+        }
     }
 }
